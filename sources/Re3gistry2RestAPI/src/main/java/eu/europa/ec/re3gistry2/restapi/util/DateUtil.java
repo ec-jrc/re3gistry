e848867505ad275ae624d61f60095515df73ce09
emanuela.epure@ext.ec.europa.eu <emanuela.epure@ext.ec.europa.eu>
2022-02-15 12:15:35 +0100

    change date format

--- sources/Re3gistry2RestAPI/src/main/java/eu/europa/ec/re3gistry2/restapi/util/DateUtil.java
+++ sources/Re3gistry2RestAPI/src/main/java/eu/europa/ec/re3gistry2/restapi/util/DateUtil.java
@@ -5,25 +5,33 @@
  */
 package eu.europa.ec.re3gistry2.restapi.util;
 
+import eu.europa.ec.re3gistry2.restapi.format.RDFFormatter;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class DateUtil {
 
     private static final Logger LOG = LogManager.getLogger(DateUtil.class.getName());
-    
-    private static DateFormat dateFormatIso8601 = new SimpleDateFormat("yyyy-MM-dd");
 
     public DateUtil() {
     }
 
-    /**
-     * Formats the given date according to ISO 8601.
-     */
-    public static String convertDate(Date date) {
-      return dateFormatIso8601.format(date);
-   }
+   public static String convertDate(Date date) {
+        try {
+            DateFormat f = new SimpleDateFormat("E MMM dd HH:mm:ss zzz yyyy");
+            Date d = f.parse(date.toString());
+            DateFormat out = new SimpleDateFormat("yyyy-MM-dd");
+            return out.format(d);
+
+        } catch (ParseException ex) {
+            java.util.logging.Logger.getLogger(RDFFormatter.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
